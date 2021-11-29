62d8b880382658dd6f6a11ebe0b8463b600b122a
Reef <reefmatson@gmail.com>
2021-11-30 12:41:57 +1300

    api is working to get blockscan urls and display on frontend

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -1,7 +1,10 @@
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import MainLayout from '../components/layouts/MainLayout';
 
 const StyledMainLayout = styled(MainLayout)`
+  text-align: center;
+
   h1 {
     margin: 3rem auto;
     text-align: center;
@@ -9,9 +12,27 @@ const StyledMainLayout = styled(MainLayout)`
 `;
 
 const Profile = () => {
+  const [urls, setUrls] = useState([]);
+
+  const getBlockscanUrls = async () => {
+    const data = await fetch(
+      '/api/get-blockscan-urls/0x0235C9D8413b4807602468Ed363dfAa8A1c5Cde2',
+    ).then((res) => res.json());
+    console.log(data.number_of_addresses_found);
+    setUrls(data.links);
+    return data.links;
+  };
+
+  useEffect(() => {
+    getBlockscanUrls();
+  }, []);
+
   return (
     <StyledMainLayout>
       <h1>Profile</h1>
+      {urls?.map((url, index) => {
+        return <p key={index}>{url}</p>;
+      })}
     </StyledMainLayout>
   );
 };
