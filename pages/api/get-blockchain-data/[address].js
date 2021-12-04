647cc05e0e34f6f494d70bd286c5d8afcf53c033
Reef <reefmatson@gmail.com>
2021-12-04 23:39:52 +1300

    disable scraping for prod

--- pages/api/get-blockchain-data/[address].js
+++ pages/api/get-blockchain-data/[address].js
@@ -55,9 +55,8 @@ const handler = async (req, res) => {
   let data = {};
 
   data =
-    (await scrapeBlockscanData(address)) ||
-    (await getBlockchainData(address)) ||
-    {};
+    // (await scrapeBlockscanData(address)) ||
+    (await getBlockchainData(address)) || {};
 
   res.setHeader('Cache-Control', 'public, max-age=900, immutable'); // cache the response for 15 minutes
   res.setHeader('X-Cache', 'HIT');
