4534977b80d9eb8cb69ba92b6b743bfe8cae34a3
Reef <reefmatson@gmail.com>
2021-12-05 00:06:03 +1300

    add chromium packages

--- pages/api/get-blockchain-data/[address].js
+++ pages/api/get-blockchain-data/[address].js
@@ -55,8 +55,9 @@ const handler = async (req, res) => {
   let data = {};
 
   data =
-    // (await scrapeBlockscanData(address)) ||
-    (await getBlockchainData(address)) || {};
+    (await scrapeBlockscanData(address)) ||
+    (await getBlockchainData(address)) ||
+    {};
 
   res.setHeader('Cache-Control', 'public, max-age=900, immutable'); // cache the response for 15 minutes
   res.setHeader('X-Cache', 'HIT');
